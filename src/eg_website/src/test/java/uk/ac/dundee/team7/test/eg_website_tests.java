b86cd297eb6bb34c3d456dd5de6e933114dd6378
Andrew Pond <a.r.pond@dundee.ac.uk>
2015-09-19 12:13:07 +0100

    Added Test Suite

---
+++ src/eg_website/src/test/java/uk/ac/dundee/team7/test/eg_website_tests.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.dundee.team7.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Andrew
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({})
+public class eg_website_tests {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
