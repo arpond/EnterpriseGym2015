fdef4dc99cf22cfde01606893d24f3cb0d8325f4
Andrew Pond <a.r.pond@dundee.ac.uk>
2015-09-21 16:50:00 +0100

    Basic view content functionality (still needs to split url for path)

--- src/eg_website/src/main/java/uk/ac/dundee/team7/eg_website/Servlet/ViewContent.java
+++ src/eg_website/src/main/java/uk/ac/dundee/team7/eg_website/Servlet/ViewContent.java
@@ -1,10 +1,38 @@
 package uk.ac.dundee.team7.eg_website.Servlet;
 
-public class ViewContent {
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import uk.ac.dundee.team7.eg_website.Store.ContentStore;
+import uk.ac.dundee.team7.eg_website.model.Content;
 
-	public void doGet() {
-		// TODO - implement ViewContent.doGet
-		throw new UnsupportedOperationException();
+
+@WebServlet(name = "Content", urlPatterns = {"/Content"})
+public class ViewContent extends HttpServlet{
+
+	@Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                String path = "dsadas"; //TODO split string and get path from current path
+                
+                Content cm = new Content();
+                ContentStore cs;
+                try
+                {
+                     cs = cm.fetchContent(path);
+                }
+                catch (Exception e)
+                {
+                    Message.message("Database error. " + e.toString(), request, response);
+                    return;
+                }
+                request.setAttribute("content", cs);
+                RequestDispatcher view = request.getRequestDispatcher("/displayContent.jsp");
+                view.include(request, response);
 	}
 
 }
