52f8e004407e7059314ddddf73ea75003e4a6847
Dragomir <pastarmasirene@abv.bg>
2015-09-22 13:22:16 +0100

    Drago - added updateEvent function (not tested) - query works

--- src/eg_website/src/main/java/uk/ac/dundee/team7/eg_website/model/Event.java
+++ src/eg_website/src/main/java/uk/ac/dundee/team7/eg_website/model/Event.java
@@ -111,7 +111,7 @@ public class Event {
       
         
         
-        ResultSet rs = null;
+       
         try{
         cs = conn.prepareCall("{call addEvent(?,?,?,?,?,?,?,?)}");
         cs.setTimestamp(1, new Timestamp(dateTime1.getMillis()));
@@ -136,9 +136,51 @@ public class Event {
 	 * 
 	 * @param event
 	 */
-	public Bool updateEvent(EventStore event) {
-		// TODO - implement Event.updateEvent
-		throw new UnsupportedOperationException();
+	public boolean updateEvent(EventStore event) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+	Class.forName("com.mysql.jdbc.Driver").newInstance();
+        DatabaseConnection dbc = new DatabaseConnection();
+        java.sql.Connection conn = dbc.connectToDB();
+        CallableStatement cs = null;
+        CallableStatement cs1 = null;
+        ContentStore contStore = event.getContent();
+         
+        int tempEventID = event.getEventID();
+        int tempContentID = contStore.getContentID();
+        DateTime tempStartTime = event.getEventStartTime();
+        String tempImageURL = event.getEventImage();
+        String tempContent = contStore.getContent();
+        int tempPoints = event.getEventValue();
+        String tempContentPath = contStore.getContentPath();
+        try{
+        ResultSet rs = null;
+        cs1 = conn.prepareCall("{call getPointTypeID(?)}");
+        int tempEventPointTypeID;
+        cs1.setString(1, event.getEventPointType());
+        cs1.execute();
+        rs = cs1.getResultSet();
+        tempEventPointTypeID = rs.getInt("typeID");
+        String tempContentTitle = contStore.getContentTitle();
+       //new Timestamp(dateTime1.getMillis()
+        
+        //contStore.getContentID()
+        cs = conn.prepareCall("{call updateEvent(?,?,?,?,?,?,?,?,?)}");
+        cs.setInt(1,tempEventID);
+        cs.setInt(2, tempContentID);
+        cs.setTimestamp(3, new Timestamp(tempStartTime.getMillis()));
+        cs.setString(4, tempImageURL);
+        cs.setString(5, tempContent);
+        cs.setInt(6, tempPoints);
+        cs.setString(7, tempContentPath);
+        cs.setInt(8, tempEventPointTypeID);
+        cs.setString(9, tempContentTitle);
+        cs.execute();
+        conn.close();
+        return true;
+        }catch(SQLException se){
+            conn.close();
+            return false;
+        }
+        
 	}
 
 	/**
