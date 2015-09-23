d71f6cc17338f1bfb6b2c7f241b9169f363f1bdf
Andrew Pond <a.r.pond@dundee.ac.uk>
2015-09-23 09:25:59 +0100

    Basic Logout Servlet

--- src/eg_website/src/main/java/uk/ac/dundee/team7/eg_website/Servlet/Logout.java
+++ src/eg_website/src/main/java/uk/ac/dundee/team7/eg_website/Servlet/Logout.java
@@ -1,10 +1,31 @@
 package uk.ac.dundee.team7.eg_website.Servlet;
 
-public class Logout {
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-	public void doGet() {
-		// TODO - implement Logout.doGet
-		throw new UnsupportedOperationException();
-	}
+@WebServlet(name = "Logout", urlPatterns = {"/Logout"})
+public class Logout extends HttpServlet{
+
+    /**
+     * Logs out the user
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session=request.getSession();
+        session.invalidate();
+        request.setAttribute("message", "You have succesfully logged out");
+        RequestDispatcher view = request.getRequestDispatcher("/message.jsp");
+        view.forward(request, response);
+    }
 
 }
