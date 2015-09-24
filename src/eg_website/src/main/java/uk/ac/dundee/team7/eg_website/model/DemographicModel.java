4d7a37fd65b16435ababa633e3a255c78af6d36b
Dragomir <pastarmasirene@abv.bg>
2015-09-24 10:54:19 +0100

    Drago - fetchInstitutions function done ( not tested ) - > query works

--- src/eg_website/src/main/java/uk/ac/dundee/team7/eg_website/model/DemographicModel.java
+++ src/eg_website/src/main/java/uk/ac/dundee/team7/eg_website/model/DemographicModel.java
@@ -5,6 +5,9 @@
  */
 package uk.ac.dundee.team7.eg_website.model;
 
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import uk.ac.dundee.team7.eg_website.Store.CountryStore;
 import uk.ac.dundee.team7.eg_website.Store.InstitutionStore;
@@ -16,8 +19,25 @@ import uk.ac.dundee.team7.eg_website.Store.StatusStore;
  */
 public class DemographicModel {
 
-    public ArrayList<InstitutionStore> fetchInstitutions() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ArrayList<InstitutionStore> fetchInstitutions() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+           Class.forName("com.mysql.jdbc.Driver").newInstance();
+        DatabaseConnection dbc = new DatabaseConnection();
+        java.sql.Connection conn = dbc.connectToDB();
+        CallableStatement cs = null;        
+        ArrayList<InstitutionStore> instList = new ArrayList<InstitutionStore>(); 
+        cs = conn.prepareCall("{call getInstitutions()}");
+        cs.execute();
+        ResultSet rs = cs.getResultSet();
+        rs.first();
+        while(rs.next())
+        {
+            InstitutionStore instStore = new InstitutionStore();
+            instStore.setInstitutionID(rs.getInt("institutionID"));
+            instStore.setInstitutionName(rs.getString("institutionName"));
+            instList.add(instStore);
+        }
+        
+        return instList;
     }
 
     public ArrayList<CountryStore> fetchCountries() {
@@ -25,6 +45,10 @@ public class DemographicModel {
     }
 
     public ArrayList<StatusStore> fetchStatuses() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public ArrayList<StatusStore> fetchDegrees() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
